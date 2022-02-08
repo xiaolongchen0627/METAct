bf4838f40eb2304fa116acdbe5a89fa40d6474d5
xiaolongchen0627 <52510143+xiaolongchen0627@users.noreply.github.com>
2022-02-08 15:30:40 -0600

    Feb8 , 2022

--- function/common_functions.py
+++ function/common_functions.py
@@ -5,7 +5,7 @@ import re
 import os 
 import pandas as pd
 
-def extractSoftclipReads(bamfile,sclipfile,keepall=False,sclip_len=20):
+def extractSoftclipReads(bamfile,sclipfile,sclip_len=20):
 	fq = ''
 	file=open(bamfile,'r')
 	file=pysam.AlignmentFile(file,'rb')
@@ -17,45 +17,40 @@ def extractSoftclipReads(bamfile,sclipfile,keepall=False,sclip_len=20):
 		qual = ''.join(map(lambda x: chr( x+33 ), read.query_qualities))
 		qname = read.qname
 		flag = read.flag
-		pos = read.pos
+		pos = int(read.pos)
 		read = str(read).split()
 		cigar = read[5]
 		num_M = re.findall("\d+[IDNSM]",cigar)
 		num_IDS = re.findall("\d+[IDS]",cigar)
-		if (len(num_IDS) >= 2) and (keepall == False ):   ######only keep reads have softclipped reads on one end and without [ID] 
-			continue
-		else : 
-			num_int = re.split("[SDNMI]", cigar)
-			num_int = num_int[0:-1]
-			num_int = list(map(int,num_int))
-
-			pos_ref = [1 for i in range(len(num_M))]
-			pos_seq = [1 for i in range(len(num_M))]
-
-			for i in range(len(num_M)):
-				if "I" in num_M[i] or 'S' in num_M[i] :
-					pos_ref[i] = 0
-				if "D" in num_M[i] or 'N' in num_M[i] :
-					pos_seq[i] = 0
-
-			pre_sum_ref = [pos_ref[i]*num_int[i] for i in range(len(num_int))]
-			cum_sum_ref = [sum(pre_sum_ref[:i])+pos for i in range(1, len(num_int)+1)]
-			cum_sum_ref = [pos] + cum_sum_ref 
-
-			pre_sum_seq = [pos_seq[i]*num_int[i] for i in range(len(num_int))]
-			cum_sum_seq = [sum(pre_sum_seq[:i]) for i in range(1, len(num_int)+1)]
-			cum_sum_seq = [0] + cum_sum_seq 
-
-			split_seq = [seq[i:j] for i, j in zip(cum_sum_seq[:-1], cum_sum_seq[1:])]
-			split_qual = [qual[i:j] for i, j in zip(cum_sum_seq[:-1], cum_sum_seq[1:])]
-
-			for i in range(len(num_M)):
-				if len(split_seq[i])>= sclip_len and 'S' in num_M[i] :
-					fq += '@%s_chr:%s_pos:%s_cigar:%s_flag:%s\n' %(qname,rname,str(cum_sum_ref[i]+1),cigar,str(flag))
-					fq += '%s\n' %split_seq[i]
-					fq += '+\n'
-					fq += '%s\n' %split_qual[i]
-
+		num_S = [int(i) for i in re.findall("(\d+)[S]",cigar)]
+		if len(num_S)>=1: 
+			if ((len(num_IDS) >= 2) or (min(num_S)>5 and len(num_S)>=2) ) :   ######only keep reads have softclipped reads on one end and without [ID] , allow maximum 5 reads if both end have that 
+				continue
+			else : 
+				num_int = re.split("[SDNMI]", cigar)
+				num_int = num_int[0:-1]
+				num_int = list(map(int,num_int))
+				pos_ref = [1 for i in range(len(num_M))]
+				pos_seq = [1 for i in range(len(num_M))]
+				for i in range(len(num_M)):
+					if "I" in num_M[i] or 'S' in num_M[i] :
+						pos_ref[i] = 0
+					if "D" in num_M[i] or 'N' in num_M[i] :
+						pos_seq[i] = 0
+				pre_sum_ref = [pos_ref[i]*num_int[i] for i in range(len(num_int))]
+				cum_sum_ref = [sum(pre_sum_ref[:i])+pos for i in range(1, len(num_int)+1)]
+				cum_sum_ref = [pos] + cum_sum_ref
+				pre_sum_seq = [pos_seq[i]*num_int[i] for i in range(len(num_int))]
+				cum_sum_seq = [sum(pre_sum_seq[:i]) for i in range(1, len(num_int)+1)]
+				cum_sum_seq = [0] + cum_sum_seq 
+				split_seq = [seq[i:j] for i, j in zip(cum_sum_seq[:-1], cum_sum_seq[1:])]
+				split_qual = [qual[i:j] for i, j in zip(cum_sum_seq[:-1], cum_sum_seq[1:])]
+				for i in range(len(num_M)):
+					if len(split_seq[i])>= sclip_len and 'S' in num_M[i] :
+						fq += '@%s_chr:%s_pos:%s_cigar:%s_flag:%s\n' %(qname,rname,str(cum_sum_ref[i]+1),cigar,str(flag))
+						fq += '%s\n' %split_seq[i]
+						fq += '+\n'
+						fq += '%s\n' %split_qual[i]
 	sclipOut.write(fq)
 	sclipOut.close()
 
@@ -76,46 +71,160 @@ def filter_hg38Sam(sam_file,psl_file,output_file):
     samfile=pysam.AlignmentFile(samfile,'r')
     output = pysam.AlignmentFile(output_file,'wb',template=samfile)
     for read in samfile.fetch():        
-        if read.has_tag("NM") and ((read.get_tag("NM") == 1) or (read.get_tag("NM") == 0)):
-            NM = read.get_tag("NM")
-            rname = samfile.getrname(read.rname)
-            qname = read.qname 
-            pos = int(read.pos)
+        if read.has_tag("NM") and ((read.get_tag("NM") == 1) or (read.get_tag("NM") == 0)) : 
             read_str = str(read).split()
             cigar = read_str[5]
-            read_l1hs_pos = int(re.findall("pos:(\d*)_cigar",read_str[0])[0])
-            subset_psl = psl[(psl["Q_Name_Chr"] == rname) & (psl["Q_Name_Start"] >= pos - 20000) & (psl["Q_Name_End"] <= pos + 20000) &
-            ( read_l1hs_pos >= psl["T_Start"] ) & (read_l1hs_pos <= psl["T_End"])]
-            read.tags=read.tags+[("L1",len(subset_psl))]            
-            output.write(read)
+            sclip = [int(i) for i in re.findall('(\d+)S',cigar)]
+            Mapped_reads_len = [int(i) for i in re.findall('(\d+)M',cigar)][0]
+            Qname = read.qname
+            Pos = int(read.pos)
+            flag = read.flag
+            BP= Pos if (flag&16 == 0 ) else Pos + Mapped_reads_len ### BreakPoints from the alignments.
+            if (not re.findall("[ID]",cigar)):	
+            	if (len(sclip)<=1):
+            		sclip_len = 0 if len(sclip) == 0 else sclip[0]
+            		if sclip_len <=3 :
+            			rname = samfile.getrname(read.rname)
+            			pos = int(read.pos)	
+            			read_l1hs_pos = int(re.findall("pos:(\d*)_cigar",read_str[0])[0])
+            			if read_l1hs_pos == 97 :  ###only extract 97SD for now 
+	            			subset_psl = psl[(psl["Q_Name_Chr"] == rname) & (psl["Q_Name_Start"] >= pos - 20000) & (psl["Q_Name_End"] <= pos + 20000) & ( read_l1hs_pos >= psl["T_Start"] ) & (read_l1hs_pos <= psl["T_End"])]
+	            			#read.tags = read.tags+[("L1",len(subset_psl))] #####
+	            			L1_within = psl[(psl["Q_Name_Chr"] == rname) & (pos >= psl["Q_Name_Start"]) & (pos <=psl["Q_Name_End"])]
+	            			read.tags = read.tags+[('BP',BP)] ######breakpoints of sclip reads
+	            			read.tags = read.tags+[("OD",":".join(subset_psl["Q_Name_ID"]))]  ########old L1s 
+	            			read.tags = read.tags+[("WI",":".join(L1_within["Q_Name_ID"]))] ###### if there is anything within the 
+	            			# L1 tag indicating there is potential a polymophic Line1 nearby.           
+	            			output.write(read)
     samfile.close()
     output.close()
 
-# ref_file='/home/xchen2/FOXR2/hgg.RNA.l1hs/SJHGG030242_D1.bam'
-# query_file='/home/xchen2/FOXR2/hgg.RNA.l1hs/hg38/SJHGG030242_D1.hg38.bam'
-# output_file='/home/xchen2/FOXR2/hgg.RNA.l1hs/hg38/SJHGG030242_D1.hg38.tmp.bam' 
+#ref_file='/home/xchen2/FOXR2/hgg.RNA.l1hs/SJHGG030242_D1.bam'
+#query_file='/home/xchen2/FOXR2/hgg.RNA.l1hs/hg38/SJHGG030242_D1.hg38.bam'
+#filter_sam='/home/xchen2/FOXR2/hgg.RNA.l1hs/hg38/SJHGG030242_D1.hg38.tmp.sam' 
+#
 
-def scoring_alignment(ref_file,query_file,output_file):
-ref = open(str(ref_file),'r')
-query = open(str(query_file),'r')
-output = open(str(output_file), 'w')
-
-ref = pysam.AlignmentFile(ref,'rb')
-query = pysam.AlignmentFile(query,'rb')
-i=0
-for queryRead in query.fetch():
-	queryRname = query.getrname(queryRead.rname)
-	queryQname = queryRead.qname
-	queryPos = int(queryRead.pos)
-	#print(queryRead.flag)
+def scoring_alignment(ref_file,query_file,filter_sam):
+	summary_file = filter_sam + '.summary.txt'
+	fastq_output = re.sub('.sam$','.fq.name.txt',filter_sam) 
 	
-	for refRead in ref.fetch(queryRname,queryPos-50,queryPos+50):
-		refQname = refRead.qname
-		refPos = int(refRead.pos)
-		print([i,queryRname,refQname,queryQname,queryRead.pos,refPos])
-	i = i + 1
-	if i==2:
-		break 
+	#summary_file.write('SeqName\tL1HS.pos\treference_breakpoints\treads_support\treads_from_different_pair\thg38_reads\tsclip_Seqs\tref_Seqnames\tref_Seqs\n')
 
+
+	ref = open(str(ref_file),'r')
+	query = open(str(query_file),'r')
+
+	filter_sam = open(str(filter_sam),'w')
+
+	ref = pysam.AlignmentFile(ref,'rb')
+	query = pysam.AlignmentFile(query,'rb')
+	filter_sam = pysam.AlignmentFile(filter_sam,'w',template=ref)
+
+	readSupportQueryBP = {}
+	readSupportRefBP = {} 
+	rcSupportQueryBP = {}
+	rcSupportRefBP = {} 
+	seqSupportQueryBP = {}
+	seqSupportRefBP = {} 
+
+	processedRef=[]
+	chrs=["chr"+str(i+1) for i in list(range(22))]
+	chrs.append('chrX')
+	chrs.append('chrY')
+	#chrs=['chrX']
+	for queryRead in query.fetch():	
+		queryRname = query.getrname(queryRead.rname)
+		if queryRname in chrs:
+			queryQname = queryRead.qname
+			queryPos = int(queryRead.pos)
+			#print(queryQname)
+			queryL1pos = [int(i) for i in re.findall('pos:(\d+)_',queryQname)][0]
+			queryL1cigar = [i for i in re.findall('_cigar:(.*)_flag:',queryQname)][0]			
+			queryL1cigar_num = re.findall("\d+[IDNSM]",queryL1cigar)
+			if len(queryL1cigar_num ) <= 4 : ##simple but useful also 
+				queryCigar = str(queryRead).split()[5]
+				queryMapped_reads_len = [int(i) for i in re.findall('(\d+)M',queryCigar)][0]
+				
+				querySeq = queryRead.query_sequence
+				queryBP = int(queryRead.get_tag('BP'))
+				queryPSL = str(queryRead.get_tag('OD'))
+				rcSupportQueryBP[''.join([queryRname,':',str(queryBP)])]=rcSupportQueryBP.setdefault(''.join([queryRname,':',str(queryBP)]),0) + 1
+				seqSupportQueryBP.setdefault(''.join([str(queryRname),':',str(queryBP)]),[]).append(str(querySeq))
+				readSupportQueryBP.setdefault(''.join([str(queryRname),':',str(queryBP)]),[]).append(str(queryQname))
+				
+				for refRead in ref.fetch(queryRname,queryBP-100,queryBP+100): ##search for reads that witin 200 bp region of the breakpoints.
+					refQname = refRead.qname
+					same_read = 0
+			
+					if not refQname in processedRef : 
+						refPos = int(refRead.pos)
+						refSeq = refRead.query_sequence
+						refCigar = str(refRead).split()[5]
+						refMapped_reads_len = sum([int(i) for i in re.findall('(\d+)[MD]',refCigar)])
+						same_read = 0  # if the reads are the same one .  
+						if (queryBP > refPos) and (queryBP < refPos + refMapped_reads_len) : 
+						# reads contain the Breakpoints. treat as prefiltered WT reads
+							if re.match(refQname,queryQname):
+								# if the reads can be perfectly mapped in the original RNA-seq data or 
+								# containes few softclipped reads 
+								same_read = 1 
+							else :
+								rcSupportRefBP[''.join([queryRname,':',str(queryBP)])]=rcSupportRefBP.setdefault(''.join([queryRname,':',str(queryBP)]),0) + 1
+								seqSupportRefBP.setdefault(''.join([str(queryRname),':',str(queryBP)]),[]).append(str(refSeq))
+								readSupportRefBP.setdefault(''.join([str(queryRname),':',str(queryBP)]),[]).append(str(refQname))
+							num_M = re.findall("\d+[IDNSM]",refCigar)
+							refRead.tags=refRead.tags+[("MH",same_read)]+[("PT",len(num_M))]+[('BP',queryBP)]+[("OD",queryPSL)]		
+							filter_sam.write(refRead)
+							#fastq_output.write(re)
+						processedRef.append(refQname)
+
+
+	filter_sam.close()
+	ref.close()
+	query.close()
+	#summary_file = 
+	#summary_file='/home/xchen2/FOXR2/hgg.RNA.l1hs/hg38/SJHGG030242_D1.hg38.tmp.summary.txt' 
+	summary_file = open(str(summary_file),'w')
+	summary_file.write('SeqName\tL1HS.pos\treference_breakpoints\treads_support\treads_from_different_pair\thg38_reads\tsclip_Seqs\tref_Seqnames\tref_Seqs\n')
+
+	for key in rcSupportQueryBP.keys():
+		if(rcSupportQueryBP[key])>=2:
+			reads = [re.sub('_chr.*','',i) for i in readSupportQueryBP[key]]
+			cigar = [re.findall('_cigar:(.*)_flag:',i)[0] for i in readSupportQueryBP[key]]
+			pos = [int(re.findall('pos:(\d+)_',i)[0]) for i in readSupportQueryBP[key]]
+			realSupport = len(list(set([re.sub('/.*','',i) for i in readSupportQueryBP[key]])))
+			
+			if key in readSupportRefBP:
+				hg38reads = rcSupportRefBP[key]
+				hg38seq = ';;'.join(list(set([re.sub('/.*','',i) for i in seqSupportRefBP[key]])))
+				hg38seqname = ';;'.join(list(set([re.sub('/.*','',i) for i in readSupportRefBP[key]])))
+			else:
+				hg38reads = 0
+				hg38seq = ''
+				hg38seqname = ''
+			for i in range(len(reads)) :
+				summary_file.write('%s\t%d\t%s\t%d\t%d\t%d\t%s\t%s\t%s\n' %(reads[i],pos[i],key,rcSupportQueryBP[key],realSupport,hg38reads,seqSupportQueryBP[key][i],hg38seqname,hg38seq))
+
+	summary_file.close()
+
+	fastq_output = open(str(fastq_output),'w')
+
+	processedRef = list(set([re.sub('\/%d+','',i) for i in processedRef])) ###remove /1 and /2 of the read name 
+	if len(processedRef)>= 1 :
+		for qname in processedRef :
+			fastq_output.write('%s\n' %qname)
+
+	fastq_output.close()
+
+					#print(len(processedRef))
+
+
+				#if same_read ==1 :
+					#summary_file.write('SeqName\tL1HS.pos\treference_chr\tbreakpoints\tref_alignment_cigar\tsclip_seq\ttotal_seq\n')
+					#summary_file.write('%s\t%d\t%s\t%s\t%s\t%s\t%s\n' %(queryQname,queryL1pos,queryRname,queryBP,refCigar,querySeq,refSeq) )
+					#i = i + 1
+					#print(refRead.tags)
+	#if i > 10:
+		#break 
 
 
--- snakefile
+++ snakefile
@@ -1,4 +1,4 @@
-configfile:"config/config.yaml"
+configfile:"config.yaml"
 
 import pandas as pd
 #from snakemake.utils import validate
@@ -15,7 +15,7 @@ def is_paired(wildcards):
     else:
         return False
 
-def Trimming(wildcards):
+def need_trim(wildcards):
     if units.loc[wildcards.sample]["need_trim"] == "Yes" :
         return True
     else: 
@@ -33,7 +33,7 @@ def hg38_mapping_results(wildcards):
 
 rule all:  
     input:
-        expand(os.path.join(config["working_dir"],"hg38","{sample}.hg38.bam.bai"),sample=units.sample_name)
+        expand(os.path.join(config["working_dir"],"trimmed","{sample}_val_1.fq.gz"),sample=units.sample_name)
 
 rule trim_galore:
     input:
@@ -45,23 +45,44 @@ rule trim_galore:
     params:
         basename = "{sample}",
         trimmed_dir = config["working_dir"]+"/trimmed/",
-        paired = is_paired
+        paired = is_paired,
+        trimmed = need_trim
     run: 
-        cmd = "trim_galore --gzip --output_dir {params.trimmed_dir} --basename {params.basename} "
-        if params.paired:
-            cmd += "--paired {input} ; touch {params.trimmed_dir}/{params.basename}_trimmed.fq.gz"
-        else :
-            cmd += " {input}; touch {params.trimmed_dir}/{params.basename}_val_1.fq.gz;"
-            cmd += "touch {params.trimmed_dir}/{params.basename}_val_2.fq.gz ;"
-        shell(cmd)
+        cmd = ""
+        if params.trimmed:
+            cmd += "trim_galore --gzip --output_dir {params.trimmed_dir} --basename {params.basename} "
+            if params.paired:
+                cmd += "--paired {input} ; touch {params.trimmed_dir}/{params.basename}_trimmed.fq.gz"
+            else :
+                cmd += " {input}; touch {params.trimmed_dir}/{params.basename}_val_1.fq.gz;"
+                cmd += "touch {params.trimmed_dir}/{params.basename}_val_2.fq.gz ;"
+        else:
+            
+            if params.paired:
+                if input[0].endswith('.gz'):                                        
+                    cmd += " ln -s {input[0]} {params.trimmed_dir}/{params.basename}_val_1.fq.gz; "
+                    cmd += " ln -s {input[1]} {params.trimmed_dir}/{params.basename}_val_2.fq.gz; "
+                else:                    
+                    cmd += " gzip -c {input[0]} > {params.trimmed_dir}/{params.basename}_val_1.fq.gz;"
+                    cmd += " gzip -c {input[1]} > {params.trimmed_dir}/{params.basename}_val_2.fq.gz;"
+                cmd += " touch {params.trimmed_dir}/{params.basename}_trimmed.fq.gz; "
+            else:
+                if input[0].endswith('.gz'):
+                    cmd += " ln -s {input[0]} {params.trimmed_dir}/{params.basename}_trimmed.fq.gz;"
+                else : 
+                    cmd += " gzip -c {input[0]} > {params.trimmed_dir}/{params.basename}_trimmed.fq;"
+                cmd += "touch {params.trimmed_dir}/{params.basename}_val_1.fq.gz;"
+                cmd += "touch {params.trimmed_dir}/{params.basename}_val_2.fq.gz;"
 
-rule align_2l1hs:
+        shell(cmd )
+
+rule align2CustomGenome:
     input:
         fq1=os.path.join(config["working_dir"],"trimmed","{sample}_val_1.fq.gz"),
         fq2=os.path.join(config["working_dir"],"trimmed","{sample}_val_2.fq.gz"),
         fqse=os.path.join(config["working_dir"],"trimmed","{sample}_trimmed.fq.gz")
     output:
-        os.path.join(config["working_dir"],"l1hs","{sample}.sam")
+        os.path.join(config["working_dir"],config["customGenomeName"],"{sample}.sam")
     params:
         bt2_thread = config['bt2_thread'],
         bt2_idx = config['bt2_l1_idx'],
@@ -75,17 +96,17 @@ rule align_2l1hs:
         cmd += "-S {output}"
         shell(cmd)
 
-rule filter_l1hs_sam_editDistance:
+rule filter_sam_editDistance:
     input:
-        rules.align_2l1hs.output
+        rules.align2CustomGenome.output
     output:
-        bam=os.path.join(config["working_dir"],"l1hs","{sample}.l1hs.bam"),
-        index=os.path.join(config["working_dir"],"l1hs","{sample}.l1hs.bam.bai")
+        bam=os.path.join(config["working_dir"],config["customGenomeName"],"{sample}.",config["customGenomeName"],".bam"),
+        bai=os.path.join(config["working_dir"],config["customGenomeName"],"{sample}.",config["customGenomeName"],".bam.bai")
     params:
         editD = config['edit_distance']
     run:
         cmd = "awk '$1 ~ /^@/ || $0 ~ /NM:i:[0-{params.editD}]\t/ ' {input} | "
-        cmd += "samtools view -Sb -F 4 - "
+        cmd += "samtools view -Sb -F 4 -q 20 - "
         cmd += " -o {output.bam} ;"
         cmd += "samtools sort {output.bam} -o {output.bam}.sort ; mv {output.bam}.sort {output.bam}; "
         cmd += "samtools index {output.bam}"
@@ -93,32 +114,18 @@ rule filter_l1hs_sam_editDistance:
 
 rule extract_sclip2fq:
     input:
-        bam=rules.filter_l1hs_sam_editDistance.output.bam
+        bam=rules.filter_sam_editDistance.output.bam
     output:
-        fq=os.path.join(config["working_dir"],"l1hs","{sample}.sclip.fq")
+        fq=os.path.join(config["working_dir"],config["customGenomeName"],"{sample}.sclip.fq")
     params:
-        all_sclip = config['all_sclip'],
         sclip_length = config['min_sclip_len']
     run:
-        cf.extractSoftclipReads(str(input.bam),str(output.fq),params.all_sclip,params.sclip_length)
+        cf.extractSoftclipReads(str(input.bam),str(output.fq),params.sclip_length)
 
-rule align_2L1_sources:
-    input :
-        rules.extract_sclip2fq.output
-    output: 
-        fq=os.path.join(config["working_dir"],"l1hs","{sample}.None_L1.fq")
-    params:
-        bt2_idx = config['bt2_l1source_idx']
-    run: 
-        ### only keep the sclip reads that failed to map to other L1 sources
-        cmd = " bowtie2 --very-sensitive-local -x {params.bt2_idx} -U {input} -S {output}.sam ;"
-        cmd += " samtools view -Sb -f 4 {output}.sam -o {output}.none_l1.bam; "
-        cmd += " picard SamToFastq -I {output}.none_l1.bam -F {output.fq} ;"
-        shell(cmd)
 
 rule realign_2hg38:
     input: 
-        rules.align_2L1_sources.output
+        rules.extract_sclip2fq.output
     output:
         os.path.join(config["working_dir"],"hg38","{sample}.hg38.sam")
     params:
@@ -134,22 +141,44 @@ rule filter_hg38_sam:
         bam = os.path.join(config["working_dir"],"hg38","{sample}.hg38.bam"),
         bai = os.path.join(config["working_dir"],"hg38","{sample}.hg38.bam.bai")
     params:
-        psl = config['L1_psl']
+        psl = config['L1_psl'],
+        focus_site = config['interestingSite'],
+        target = config['customGenomeName']
     run:
-        cf.filter_hg38Sam(input,params.psl,str(output.bam)),
+        cf.filter_hg38Sam(input,params.psl,str(output.bam),params.focus_site,params.target),
         cmd = 'samtools sort {output.bam} -o {output.bam}.sort ; mv {output.bam}.sort {output.bam}; '
         cmd += 'samtools index {output.bam} ' 
         shell(cmd)
 
 rule score_sam:
     input:
-        sclipSam = rules.filter_hg38_sam.output,
+        sclipSam = rules.filter_hg38_sam.output.bam,
         refSam = hg38_mapping_results
     output:
-        os.path.join(config["working_dir"],"hg38","{sample}.hg38.score.txt")
+        sam = os.path.join(config["working_dir"],"hg38","{sample}.hg38.rev.sam"),
+        fqname = os.path.join(config["working_dir"],"hg38","{sample}.hg38.rev.fq.name.txt")
     run:
-        cf.score_hg38Sam(sclipSam,refSam,str(output))
+        cf.scoring_alignment(input.refSam,input.sclipSam,str(output.sam))
         
+rule get_fastq_for_validation:
+    input :
+        fq1=rules.align2CustomGenome.input.fq1,
+        fq2=rules.align2CustomGenome.input.fq2,
+        fqname=rules.score_sam.output.fqname
+    params:
+        paired = is_paired
+    output: 
+        fq1 = os.path.join(config["working_dir"],"hg38","{sample}.hg38.rev.r1.fq"),
+        fq2 = os.path.join(config["working_dir"],"hg38","{sample}.hg38.rev.r2.fq")
+    run:
+        cmd = ' '
+        if params.paired:
+            cmd +=" zcat {input.fq1} | grep -f {input.fqname} -A 3 -h  --no-group-separator > {output.fq1};"
+            cmd += " zcat  {input.fq2}  | grep -f {input.fqname} -A 3 -h  --no-group-separator> {output.fq2};"
+        else:
+            cmd += "touch {output.fq1} ;"
+            cmd += "touch {output.fq2} "
+        shell(cmd)
 
 
         
